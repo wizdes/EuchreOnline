846c7894b828c047adb7300c19240ff6e6b6b637
wizdes <yl2326@cornell.edu>
2013-12-29 18:19:04 -0500

    added rooms servlet and fixed an issue with loading the servlets

---
+++ EuchreOnline/src/com/rdxonline/euchre/Room.java
@@ -0,0 +1,23 @@
+package com.rdxonline.euchre;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Room {
+	private Set<String> members;
+	private String roomName;
+	
+	public Room(String name) {
+		members = new HashSet<String>();
+		roomName = name;
+	}
+	
+	public void addMembers(String user){
+		members.add(user);
+	}
+	
+	public void removeMembers(String user){
+		members.remove(user);
+	}
+}
